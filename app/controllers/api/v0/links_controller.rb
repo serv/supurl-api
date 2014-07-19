dac6ff5a0a85179c3d92ac38c43e4d1987f3788c
Jason Kim <active_red@hotmail.com>
2014-07-19 00:13:01 -0700

    close #12: Day 7-3: CORS support for Link

--- app/controllers/api/v0/links_controller.rb
+++ app/controllers/api/v0/links_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V0
     class LinksController < ApplicationController
+      before_action :set_headers
+
       def show
         @link = Link.find(params[:id])
         render json: @link
@@ -10,6 +12,13 @@ module Api
 
         def link_params
           params.require(:status).permit(:title, :href, :comment)
+        end
+
+        def set_headers
+          headers['Access-Control-Allow-Origin'] = '*'
+          headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+          headers['Access-Control-Request-Method'] = '*'
+          headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
         end
     end
   end
