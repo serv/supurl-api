44a18a87a64c9271392bed299d4fca9620f931d2
Jason Kim <active_red@hotmail.com>
2014-07-18 22:03:50 -0700

    close #11: Day 7-2: get a link

---
+++ app/controllers/api/v0/links_controller.rb
@@ -0,0 +1,16 @@
+module Api
+  module V0
+    class LinksController < ApplicationController
+      def show
+        @link = Link.find(params[:id])
+        render json: @link
+      end
+
+      private
+
+      def link_params
+        params.require(:status).permit(:title, :href, :comment)
+      end
+    end
+  end
+end
