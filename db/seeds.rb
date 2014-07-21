d72f8f94e889578ecfbe8b44bcb4e548545ab26e
Jason Kim <active_red@hotmail.com>
2014-07-20 21:32:52 -0700

    close #17: Day 9: Test: Use faker to generate fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create links
+100.times do |i|
+  link = Link.new(title: Faker::Lorem.sentence,
+                  href: Faker::Internet.url,
+                  comment: Faker::Lorem.paragraph)
+  link.save
+end
