18d40caa92430314f319e2405a3b27ed39be6a39
Jason Kim <active_red@hotmail.com>
2014-08-14 01:28:21 -0700

    close #43: Day 32-3: Remove and add back username and email attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140814042850) do
+ActiveRecord::Schema.define(version: 20140814082415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,5 +45,8 @@ ActiveRecord::Schema.define(version: 20140814042850) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
