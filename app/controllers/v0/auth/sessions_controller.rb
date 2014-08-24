0863d6f42e434ed55cffb8adeb5aa3a1936908ef
Jason Kim <active_red@hotmail.com>
2014-08-24 01:33:53 -0700

    close #51: Day 37-2: Sign in: email or username sign in option

--- app/controllers/v0/auth/sessions_controller.rb
+++ app/controllers/v0/auth/sessions_controller.rb
@@ -1,14 +1,28 @@
 class V0::Auth::SessionsController < ApplicationController
   def sign_in
-    @user = User.new
   end
 
   def create
+    email_or_username = is_email_or_username(session_params[:email_username])
+
+    if email_or_username == :email
+      @user = User.find_by(email: session_params[:email_username])
+    elsif email_or_username == :username
+      @user = User.find_by(username: session_params[:email_username])
+    end
   end
 
   private
 
-    def user_params
-      params.require(:user).permit(:email, :password)
+    def session_params
+      params.require(:session).permit(:email_username, :password)
+    end
+
+    def is_email_or_username(email_or_username)
+      if email_or_username =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+        :email
+      else
+        :username
+      end
     end
 end
