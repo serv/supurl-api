edd421e864632573c47a1bc3e0b0a62c632d4878
Jason Kim <active_red@hotmail.com>
2014-08-24 23:57:35 -0700

    client on create: api_key and secret

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,13 @@ class Client < ActiveRecord::Base
   has_many :access_codes
   has_many :refresh_codes
   has_many :authorization_codes
+
+  before_validation :setup
+
+  protected
+
+    def setup
+      self.api_key = SecureRandom.base64(16)
+      self.api_secret = SecureRandom.base64(64)
+    end
 end
